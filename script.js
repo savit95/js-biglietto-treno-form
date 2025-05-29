2f766f3d15e7622d0f3e773b0245a0d3e121fe10
savit95 <salvatorevitiello95@gmail.com>
2025-05-29 22:00:45 +0200

    Selected and assigned form elements to constants

---
+++ script.js
@@ -0,0 +1,11 @@
+const ticketForm = document.getElementById('ticket-form');
+console.log(ticketForm);
+const numberKm = document.getElementById('number-km');
+console.log(numberKm);
+const dealPassenger = document.getElementById('deal-passenger');
+console.log(dealPassenger);
+const titlePriceTicket= document.getElementById('title-price-ticket');
+
+
+
+
