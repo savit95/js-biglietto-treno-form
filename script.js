4132063767d5058d81cc25566b264b6690ace029
savit95 <salvatorevitiello95@gmail.com>
2025-05-29 22:19:38 +0200

    Deal calculation implemented

--- script.js
+++ script.js
@@ -9,9 +9,24 @@ const titlePriceTicket= document.getElementById('title-price-ticket');
 
 ticketForm.addEventListener('submit',function(event){
         event.preventDefault();
-        let kilometers = numberKm.value;
-        console.log(kilometers);
+        let kilometres = numberKm.value;
+        console.log(kilometres);
         let deal = dealPassenger.value;
         console.log(deal);
+
+        let price = 0.21 * kilometres;
+
+        if (deal === 'studentdeal') {
+            reduction=20 * price/100;
+            price -= reduction;
+        } else if(deal === 'overdeal'){
+            reduction=40 * price/100;
+            price -= reduction;
+        }       
+
+
+
+        console.log(price);
+
     }
 )
